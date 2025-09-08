62c1f91d3378ba146d761bb2ef3fe5c8dbce4235
JungNamYoung <kmiss75@daum.net>
2025-09-08 18:49:55 +0900

    리팩토링2

--- src/main/java/com/web/controller/UserController.java
+++ src/main/java/com/web/controller/UserController.java
@@ -10,6 +10,7 @@ import haru.annotation.di.Autowired;
 import haru.annotation.di.Resource;
 import haru.annotation.mvc.Controller;
 import haru.annotation.mvc.RequestMapping;
+import haru.kitten.MiniHttpServletRequest;
 import haru.kitten.MiniHttpSession;
 import haru.logger.LoggerManager;
 import haru.model.Model;
@@ -26,12 +27,16 @@ public class UserController {
   public UserService userServiceExt;
 
   @RequestMapping({"/selectUser.do", "/selectUserExt.do"})
-  public String selectUser(Model model, MiniHttpSession session) {
+  public String selectUser(Model model, MiniHttpSession session, MiniHttpServletRequest request) {
 
     logger.info("selectUser()");
     
-    logger.info("id : " + session.getId());
+    logger.info("id : #1 : " + session.getId());
 
+    MiniHttpSession sess = (MiniHttpSession)request.getAttribute("miniHttpSession");
+    
+    logger.info("id : #2 : " + sess.getId());
+    
     List<UserVo> result = userServiceExt.selectUserList(null);
 
     model.addAttribute("author", "david");
