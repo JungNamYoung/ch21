db2a99dce8e8587bf5af16aec13289af4527aa26
JungNamYoung <kmiss75@daum.net>
2025-11-04 10:52:55 +0900

    edit logging

--- src/main/java/haru/logging/LoggerManager.java
+++ src/main/java/haru/logging/LoggerManager.java
@@ -1,17 +1,10 @@
 package haru.logging;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.Date;
 import java.util.logging.ConsoleHandler;
-import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
-
-import haru.constants.Define;
-import haru.core.bootstrap.MiniServletContainer;
 
 public class LoggerManager {
   private static final String LOG_FORMAT = "[haru][%1$s] %2$tT | %4$s | %5$s | %3$s";
@@ -32,31 +25,7 @@ public class LoggerManager {
     consoleHandler.setLevel(Level.ALL);
     logger.addHandler(consoleHandler);
     logger.setLevel(Level.ALL);
-
-    String logDirPath = MiniServletContainer.getRealPath(Define.STR_BLANK) + Define.WEB_INF + "/logs";
-
-    File logDir = new File(logDirPath);
-    if (!logDir.exists()) {
-      if (logDir.mkdirs()) {
-        System.out.println("Log directory created : " + logDirPath);
-      } else {
-        System.err.println("Failed to create log directory : " + logDirPath);
-      }
-    }
-
-    try {
-      FileHandler fileHandler = new FileHandler(logDirPath + "/application.log", true);
-      fileHandler.setFormatter(new SimpleFormatter() {
-        @Override
-        public String format(LogRecord record) {
-          return formatLog(record);
-        }
-      });
-      fileHandler.setLevel(Level.ALL);
-      logger.addHandler(fileHandler);
-    } catch (IOException e) {
-      System.err.println("Failed to initialize log file: " + e.getMessage());
-    }
+    
     return logger;
   }
 
